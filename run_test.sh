8ea4c9f98373f2e48cb5d96890cf06784bc50204
darkmarbo <darkmarbo@163.com>
2017-07-17 15:07:22 +0800

    v2.1 ok

--- run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#cat text_pro/* |sort|uniq > text_pro_sort/text_ug_all.uniq  
-
-head -310000 text_pro.all.count.sort | awk -F"\t" '{if(length($0)<50){print $2}}' > ttt
-head -300000 ttt > dict.ug.300000 
